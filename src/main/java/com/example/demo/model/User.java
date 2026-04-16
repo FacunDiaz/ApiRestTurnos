0eac8e37ab4640d9d7b6c13472a30e80d4f309df
Facundo D <facundomartin05@gmail.com>
2026-04-16 19:34:20 -0300

    create enum healthcareSystem and user model with basic data

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import com.example.demo.enums.ObraSocial;
+
+import java.util.Date;
+
+public class Usuario {
+    private String name;
+    private String lastname;
+    private long dni;
+    private String direction;
+    private Date bornDate;
+    private ObraSocial healthcareSys;
+    
+}
