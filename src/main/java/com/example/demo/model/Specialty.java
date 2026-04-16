65c2a9c60e1f057e93c59cce17da1b8c78a35a1b
Facundo D <facundomartin05@gmail.com>
2026-04-16 20:42:44 -0300

    advanced annotations added to specialty model class

--- src/main/java/com/example/demo/model/Specialty.java
+++ src/main/java/com/example/demo/model/Specialty.java
@@ -2,6 +2,9 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "Specialities")
 public class Specialty {
@@ -10,4 +13,44 @@ public class Specialty {
     private long idSpecialty;
     private String name;
     //private cantidad de médicos
+    @OneToMany(mappedBy = "specialty")
+    private List<Doctor> doctorList;
+
+    public Specialty(String name, List<Doctor> doctorList, long idSpecialty) {
+        this.idSpecialty = idSpecialty;
+        this.name = name;
+        this.doctorList = new ArrayList<>();
+    }
+
+    public long getIdSpecialty() {
+        return idSpecialty;
+    }
+
+    public void setIdSpecialty(long idSpecialty) {
+        this.idSpecialty = idSpecialty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Doctor> getDoctorList() {
+        return doctorList;
+    }
+
+    public void setDoctorList(List<Doctor> doctorList) {
+        this.doctorList = doctorList;
+    }
+
+    public void addToDoctorList(Doctor doctor) {
+        this.doctorList.add(doctor);
+    }
+
+    public void removeFromDoctorList(Doctor doctor){
+        this.doctorList.remove(doctor); 
+    }
 }
