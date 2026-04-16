c63d54020be15daabb62132eb93076c11ebb80a0
Facundo D <facundomartin05@gmail.com>
2026-04-16 20:33:23 -0300

    annotations added to encounter model class

--- src/main/java/com/example/demo/model/Encounter.java
+++ src/main/java/com/example/demo/model/Encounter.java
@@ -13,4 +13,60 @@ public class Encounter {
 
     private Date date;
     private String additionalComments;
+
+    @ManyToOne
+    @JoinColumn(name = "id_User")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_User")
+    private Doctor doctor;
+
+    public Encounter(long idEncounter, Date date, String additionalComments, User user, Doctor doctor) {
+        this.idEncounter = idEncounter;
+        this.date = date;
+        this.additionalComments = additionalComments;
+        this.user = user;
+        this.doctor = doctor;
+    }
+
+    public long getIdEncounter() {
+        return idEncounter;
+    }
+
+    public void setIdEncounter(long idEncounter) {
+        this.idEncounter = idEncounter;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getAdditionalComments() {
+        return additionalComments;
+    }
+
+    public void setAdditionalComments(String additionalComments) {
+        this.additionalComments = additionalComments;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 }
