9292b72504da13aee82f78a321d8f3e2432dabee
Facundo D <facundomartin05@gmail.com>
2026-04-16 20:38:31 -0300

    advanced persistence annotations added to doctor model class

--- src/main/java/com/example/demo/model/Doctor.java
+++ src/main/java/com/example/demo/model/Doctor.java
@@ -2,6 +2,9 @@ package com.example.demo.model;
 
 
 import jakarta.persistence.*;
+import org.springframework.beans.factory.BeanRegistry;
+
+import java.util.List;
 
 @Entity
 @Table(name = "Doctors")
@@ -13,4 +16,77 @@ public class Doctor {
     private String dni;
     private int yearsExp;
     private String description;
+
+    @OneToMany(mappedBy = "doctor")
+    private List<Encounter> encounterList;
+
+    @ManyToOne
+    @JoinColumn(name = "id_Specialty")
+    private Specialty specialty;
+
+    public Doctor(long idDoctor, String name, String dni, int yearsExp, String description, List<Encounter> encounterList, Specialty specialty) {
+        this.idDoctor = idDoctor;
+        this.name = name;
+        this.dni = dni;
+        this.yearsExp = yearsExp;
+        this.description = description;
+        this.encounterList = encounterList;
+        this.specialty = specialty;
+    }
+
+    public long getIdDoctor() {
+        return idDoctor;
+    }
+
+    public void setIdDoctor(long idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getYearsExp() {
+        return yearsExp;
+    }
+
+    public void setYearsExp(int yearsExp) {
+        this.yearsExp = yearsExp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Encounter> getEncounterList() {
+        return encounterList;
+    }
+
+    public void setEncounterList(List<Encounter> encounterList) {
+        this.encounterList = encounterList;
+    }
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
 }
